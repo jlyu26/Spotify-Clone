77a1e487a0625369d7636e7676abdb679b5ddbe4
J.Lyu <jlyu@wpi.edu>
2018-04-14 20:39:21 -0400

    Update progress bar and volume.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,50 @@
 var currentPlaylist = [];
 var audioElement;
+var mouseDown = false;
 
+
+function formatTime(seconds) {
+	var time = Math.round(seconds);
+	var minutes = Math.floor(time / 60); // rounds down
+	var seconds = time - minutes * 60;
+
+	var extraZero = (seconds < 10) ? "0" : "";
+
+	return minutes + ":" + extraZero + seconds;
+}
+
+function updateTimeProgressBar(audio) {
+	$(".progressTime.current").text(formatTime(audio.currentTime));
+	$(".progressTime.remaining").text(formatTime(audio.duration - audio.currentTime));
+
+	var progress = audio.currentTime / audio.duration * 100;
+	$(".playbackBar .progress").css("width", progress + "%");
+}
+
+function updateVolumeProgressBar(audio) {
+	var volume = audio.volume * 100;
+	$(".volumeBar .progress").css("width", volume + "%");
+}
 
 function Audio() {
-
+	// 'this' refered to the object the event was called on, the 'audio' object
 	this.currentlyPlaying;
 	this.audio = document.createElement('audio');
+
+	this.audio.addEventListener("canplay", function() {	// <audio> element has 'canplay' event
+		var duration = formatTime(this.duration);
+		$(".progressTime.remaining").text(duration);
+	});
+
+	this.audio.addEventListener("timeupdate", function() {
+		if (this.duration) {
+			updateTimeProgressBar(this);
+		}
+	});
+
+	this.audio.addEventListener("volumechange", function() {
+		updateVolumeProgressBar(this);
+	});
 
 	this.setTrack = function(track) {
 		this.currentlyPlaying = track;
@@ -18,6 +57,10 @@ function Audio() {
 
 	this.pause = function() {
 		this.audio.pause();
+	}
+
+	this.setTime = function(seconds) {
+		this.audio.currentTime = seconds;
 	}
 
 }
--- includes/nowPlayingBar.php
+++ includes/nowPlayingBar.php
@@ -16,7 +16,66 @@
 		currentPlaylist = <?php echo $jsonArray; ?>;
 		audioElement = new Audio();
 		setTrack(currentPlaylist[0], currentPlaylist, false);
+		updateVolumeProgressBar(audioElement.audio);
+
+		$("#nowPlayingBarContainer").on("mousedown touchstart mousemove touchmove", function(e) {
+			e.preventDefault();
+		});
+
+		//// Dragging the process bar on click
+
+		$(".playbackBar .progressBar").mousedown(function() {
+			mouseDown = true;
+		});
+
+		$(".playbackBar .progressBar").mousemove(function(e) {
+			if (mouseDown) {
+				// set time of song depending on position of mouse
+				// 'this' refers the object that calls the event, which is .progressBar
+				timeFromOffset(e, this);	
+			}
+		});
+
+		$(".playbackBar .progressBar").mouseup(function(e) {
+			timeFromOffset(e, this);	
+		});
+
+		//// Controlling the volum
+
+		$(".volumeBar .progressBar").mousedown(function() {
+			mouseDown = true;
+		});
+
+		$(".volumeBar .progressBar").mousemove(function(e) {
+			if (mouseDown) {
+				
+				var percentage = e.offsetX / $(this).width();
+				
+				if (percentage >= 0 && percentage <= 1) {
+					audioElement.audio.volume = percentage;
+				}
+			}
+		});
+
+		$(".volumeBar .progressBar").mouseup(function(e) {
+			var percentage = e.offsetX / $(this).width();
+				
+			if (percentage >= 0 && percentage <= 1) {
+				audioElement.audio.volume = percentage;
+			}
+		});
+
+		$(document).mouseup(function() {
+			mouseDown = false;
+		})
+
 	});
+
+	function timeFromOffset(mouse, progressBar) {
+		var percentage = mouse.offsetX / $(progressBar).width() * 100;
+		var seconds = audioElement.audio.duration * (percentage / 100);
+		audioElement.setTime(seconds);
+	}
 
 
 	function setTrack(trackId, newPlaylist, play) {
@@ -88,8 +147,6 @@
 
 				</div>
 
-
-
 			</div>
 		</div>
 
@@ -138,10 +195,7 @@
 
 					<span class="progressTime remaining">0.00</span>
 
-
 				</div>
-
-
 			</div>
 
 
@@ -162,9 +216,6 @@
 
 			</div>
 		</div>
-
-
-
 
 	</div>
 
